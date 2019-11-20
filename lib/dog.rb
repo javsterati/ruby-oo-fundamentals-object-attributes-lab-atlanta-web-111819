9cd97b7f49a1d37227151b4b08c71a214b2992a4
Javi <javsteratii@gmail.com>
2019-11-19 22:38:47 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+    def initialize
+        @name = name 
+        @breed = breed
+    end 
+
+    def name 
+        @name
+    end 
+    
+    def breed
+        @breed
+    end 
+    
+    def name= (names)
+        @name= names
+    end 
+
+    def breed= (dogbreed)
+        @breed = dogbreed
+    end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+    def initialize 
+        @name = name
+        @job
+    end 
+
+    def name
+        @name
+    end 
+
+    def job
+        @job
+    end 
+
+    def name= (personname)
+        @name = personname
+    end
+
+    def job= (jobs)
+        @job = jobs
+    end 
+
+end
